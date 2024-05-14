cebbe4835e6993843c504125ee1feca87a6585ec
tolga.basturk <tolga.basturk@tubitak.gov.tr>
2024-05-14 23:48:57 +0300

    project update

--- dql_cartpole.py
+++ dql_cartpole.py
@@ -21,7 +21,7 @@ class DQLAgent:
         pass
 
     def act(self, state):
-        # acting
+        # acting explore or explotation
         pass
 
     def replay(self, batch_size):
@@ -34,27 +34,43 @@ class DQLAgent:
 if __name__ == "__main__":
 
     # initialize env and agent
+    env = gym.make("CartPole-v0")
+    agent = DQLAgent(env)
 
+    batch_size = 16
     episodes = 100
     for e in range(episodes):
 
         # initialize environment
+        state = env.reset()
 
+        state = np.reshape(state, (1,-1))
+        time = 0
         while True:
 
             # act
+            action = agent.act(state) #select an action
 
             # step
+            next_state, reward, done, _ = env.step(action)
+            next_state = np.reshape(next_state, [1,4])
 
-            # remember
+            # remember /storage
+            agent.remember(state, action, reward, next_state, done)
 
             # update state
+            state = next_state
 
             # replay
+            agent.replay(batch_size)
 
             #adjust epsilon
+            agent.adaptiveEGreedy()
+
+            time += 1
 
             if done:
+                print("Episode: {}, time: {}".format(e,time))
                 break
 
 
