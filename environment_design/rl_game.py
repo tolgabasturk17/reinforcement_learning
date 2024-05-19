27c222ff4093e8990ec8ad65a5abbe2a39e75542
tolga.basturk <tolga.basturk@tubitak.gov.tr>
2024-05-20 00:08:56 +0300

    project update

--- environment_design/rl_game.py
+++ environment_design/rl_game.py
@@ -89,10 +89,10 @@ class Enemy(pygame.sprite.Sprite):
 
 class DQLAgent:
 
-    def __init__(self, env):
+    def __init__(self):
         # parameters / hyper parameters
-        self.state_size = 4
-        self.action_size = 3
+        self.state_size = 4 # distance[(playerx - enemy1x), (playery - enemy1y), (playerx - enemy2x), (playery - enemy2y)]
+        self.action_size = 3 # right, left, no move
         self.gamma = 0.95
         self.learning_rate = 0.001
         self.epsilon = 1
@@ -104,7 +104,7 @@ class DQLAgent:
     def build_model(self):
         # neural network for deep q learning
         model = keras.Sequential()
-        model.add(Dense(48, input_dim = self.state_size, activation="tanh"))
+        model.add(Dense(48, input_dim = self.state_size, activation="relu"))
         model.add(Dense(self.action_size, activation="linear"))
         model.compile(loss="mse", optimizer = Adam(learning_rate = self.learning_rate))
         return model
@@ -114,7 +114,11 @@ class DQLAgent:
         self.memory.append((state, action, reward, next_state, done))
 
     def act(self, state):
-        pass
+        state = np.array(state).reshape(1, -1)
+        if np.random.rand() < self.epsilon:
+            return random.randrange(self.action_size) # env.action_space.sample()
+        act_values = self.model.predict(state)
+        return np.argmax(act_values[0])
 
     def replay(self, batch_size):
         # training
@@ -123,14 +127,18 @@ class DQLAgent:
 
         minibatch = random.sample(self.memory, batch_size)
         for state, action, reward, next_state, done in minibatch:
+            next_state = np.array(next_state).reshape(1, -1)
+            state = np.array(state).reshape((1, -1))
             if done:
                 target = reward
             else:
+
                 target = reward + self.gamma * np.amax(self.model.predict(next_state)[0])
+
 
             trained_target = self.model.predict(state)
             trained_target[0][action] = target
-            self.model.fit(state, trained_target, verbose=0)
+            self.model.fit(state, trained_target, epochs=1, verbose=0)
 
     def adaptiveEGreedy(self):
         if self.epsilon > self.epsilon_min:
@@ -158,6 +166,10 @@ class Env(pygame.sprite.Sprite):
 
         self.agent = DQLAgent()
 
+    def findDistance(self, a, b):
+        d = a-b
+        return d
+
     def step(self, action):
         state_list = []
 
@@ -169,47 +181,108 @@ class Env(pygame.sprite.Sprite):
         next_player_state = self.player.getCoordinate()
         next_m1_state = self.enemy1.getCoordinate()
         next_m2_state = self.enemy2.getCoordinate()
-        # find distance
 
+        # find distance
+        state_list.append(self.findDistance(next_player_state[0], next_m1_state[0]))
+        state_list.append(self.findDistance(next_player_state[1], next_m1_state[1]))
+        state_list.append(self.findDistance(next_player_state[0], next_m2_state[0]))
+        state_list.append(self.findDistance(next_player_state[1], next_m2_state[1]))
+
+        return state_list
     # reset
     def initialState(self):
-        pass
+        pygame.sprite.Sprite.__init__(self)
+        self.all_sprite = pygame.sprite.Group()
+        self.enemy = pygame.sprite.Group()
+        self.player = Player()
+        self.all_sprite.add(self.player)
+        self.enemy1 = Enemy()
+        self.enemy2 = Enemy()
+        self.all_sprite.add(self.enemy1)
+        self.all_sprite.add(self.enemy2)
+        self.enemy.add(self.enemy1)
+        self.enemy.add(self.enemy2)
+
+        self.reward = 0
+        self.total_reward = 0
+        self.done = False
+
+        state_list = []
+
+        # get coordinate
+        player_state = self.player.getCoordinate()
+        m1_state = self.enemy1.getCoordinate()
+        m2_state = self.enemy2.getCoordinate()
+
+        state_list.append(self.findDistance(player_state[0], m1_state[0]))
+        state_list.append(self.findDistance(player_state[1], m1_state[1]))
+        state_list.append(self.findDistance(player_state[0], m2_state[0]))
+        state_list.append(self.findDistance(player_state[1], m2_state[1]))
+
+        return state_list
 
     def run(self):
-        pass
+        # game loop
+        state = self.initialState()
+        running = True
+        batch_size = 24
+        while running:
+            self.reward = 2
+            # keep loop running at the right speed
+            clock.tick(FPS)
 
+            # process input
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
 
-# initialize pygame and create window
-pygame.init()
-screen = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("BASTURK'S FIRST REINFORCEMENT LEARNING GAME")
-clock = pygame.time.Clock()
+            #update
+            action = self.agent.act(state)
+            next_state = self.step(action)
+            self.total_reward += self.reward
 
+            #self.player.update(action)
 
-# game loop
-running = True
-while running:
-    # keep loop running at the right speed
-    clock.tick(FPS)
+            hits = pygame.sprite.spritecollide(self.player, self.enemy, False, pygame.sprite.collide_circle)
+            if hits:
+                self.reward = -150
+                self.total_reward += self.reward
+                self.done = True
+                running = False
+                print("Total reward: ", self.total_reward)
 
-    # process input
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
+            self.agent.remember(state, action, self.reward, next_state, self.done)
 
-    # update
-    all_sprite.update()
+            # update
+            state = next_state
 
-    hits = pygame.sprite.spritecollide(player, enemy, False, pygame.sprite.collide_circle)
-    if hits:
-        running = False
-        print("Game Over!")
+            # training
+            self.agent.replay(batch_size)
 
-    # draw / render(show)
-    screen.fill(GREEN)
-    all_sprite.draw(screen)
+            # epsilon greedy
+            self.agent.adaptiveEGreedy()
 
-    # flip display after drawing
-    pygame.display.flip()
+            # draw / render(show)
+            screen.fill(GREEN)
+            self.all_sprite.draw(screen)
+            pygame.display.flip()
 
-pygame.quit()
+        pygame.quit()
+
+if __name__ == "__main__":
+    env = Env()
+    liste = []
+    t = 0
+    while True:
+        t += 1
+        print("Episode: ", t)
+        liste.append(env.total_reward)
+
+        # initialize pygame and create window
+        pygame.init()
+        screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption("BASTURK'S FIRST REINFORCEMENT LEARNING GAME")
+        clock = pygame.time.Clock()
+
+        env.run()
+
