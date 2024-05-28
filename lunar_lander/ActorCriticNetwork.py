56d7f476913e03a1be9436eec7c802147d517dbb
tolga.basturk <tolga.basturk@tubitak.gov.tr>
2024-05-28 23:57:52 +0300

    project update

--- lunar_lander/ActorCriticNetwork.py
+++ lunar_lander/ActorCriticNetwork.py
@@ -1,3 +1,26 @@
 import numpy as np
 import torch as T
 import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+class ActorCriticNetwork(nn.Module):
+    def __init__(self, lr, input_dims, n_actions, fc1_dims=256, fc2_dims=256):
+        super(ActorCriticNetwork, self).__init__()
+        self.fc1 = nn.Linear(*input_dims,fc1_dims)
+        self.fc2 = nn.Linear(fc1_dims, fc2_dims)
+        # two different outputs
+        # policy output n actions
+        self.pi = nn.Linear(fc2_dims, n_actions)
+        # value function one output
+        self.v = nn.Linear(fc2_dims, 1)
+        self.optimizer = optim.Adam(self.parameters(), lr=lr)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        pi = self.pi(x)
+        v = self.v(x)
+
+        return (pi, v)
