5ebe2546bbe5613f47b7e50b0b9190e9f14ed2b3
tolga.basturk <tolga.basturk@tubitak.gov.tr>
2024-05-30 08:53:54 +0300

    project update

---
+++ lunar_lander/ActorCriticAgent.py
@@ -0,0 +1,47 @@
+import torch as T
+import torch.nn.functional as F
+
+from ActorCriticNetwork import ActorCriticNetwork
+
+class ActorCriticAgent():
+
+    def __init__(self, lr, input_dims, fc1_dims, fc2_dims, n_actions, gamma=0.99):
+        self.gamma = gamma
+        self.lr = lr
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.actor_critic = ActorCriticNetwork(lr, input_dims, n_actions, fc1_dims, fc2_dims)
+
+        self.log_prob = None
+
+    def choose_action(self, observation):
+
+        state = T.tensor([observation], dtype=T.float).to(self.actor_critic.device)
+        probabilities, _ = self.actor_critic.forward(state)
+        probabilities = F.softmax(probabilities, dim=1)
+        action_probs = T.distributions.Categorical(probabilities)
+        action = action_probs.sample()
+        log_prob = action_probs.log_prob(action)
+        self.log_prob = log_prob
+
+        return action.item() #action ın kendini değil item() metodunu döndürüyoruz çünkü action bir Tensor
+
+    def learn(self, state, reward, state_, done):
+        self.actor_critic.optimizer.zero_grad()
+
+        state = T.tensor([state], dtype=T.float).to(self.actor_critic.device)
+        state_= T.tensor([state_], dtype=T.float).to(self.actor_critic.device)
+        reward = T.tensor(reward, dtype=T.float).to(self.actor_critic.device)
+
+        _,critic_value = self.actor_critic.forward(state)
+        _,critic_value_ = self.actor_critic.forward(state_)
+
+        delta = reward + self.gamma*critic_value_*(1-int(done)) - critic_value
+
+        actor_loss = -self.log_prob*delta
+        critic_loss = delta**2
+
+        (actor_loss + critic_loss).backward()
+        self.actor_critic.optimizer.step()
+
+
--- lunar_lander/ActorCriticNetwork.py
+++ lunar_lander/ActorCriticNetwork.py
@@ -16,7 +16,7 @@ class ActorCriticNetwork(nn.Module):
         self.v = nn.Linear(fc2_dims, 1)
         self.optimizer = optim.Adam(self.parameters(), lr=lr)
         self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
-
+        self.to(self.device)
     def forward(self, state):
         x = F.relu(self.fc1(state))
         x = F.relu(self.fc2(x))
